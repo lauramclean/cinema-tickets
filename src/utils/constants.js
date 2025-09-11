c54eaa587fb283b2d08b7c17cf88a4d9f7326f87
Laura Mclean <lauramclean86@gmail.com>
2025-09-11 15:13:31 +0100

    refactor: replace hardcoded types with const definitions

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,23 @@
 /**
  * List of Defintions
  */
+export const TICKET_TYPE_ADULT = "ADULT";
 
-export const TICKET_TYPES = ["ADULT", "CHILD", "INFANT"]; //mapped from TicketTypeRequest
+export const TICKET_TYPE_CHILD = "CHILD";
+
+export const TICKET_TYPE_INFANT = "INFANT";
+
+export const TICKET_TYPES = [TICKET_TYPE_ADULT, TICKET_TYPE_CHILD, TICKET_TYPE_INFANT]; //mapped from TicketTypeRequest
 
 export const TICKET_TYPE_MAP = [
-  { type: "ADULT",  price: 25, requireSeat: true },
-  { type: "CHILD",  price: 15, requireSeat: true },
-  { type: "INFANT", price: 0,  requireSeat: false }
+  { type: TICKET_TYPE_ADULT,  price: 25, requireSeat: true },
+  { type: TICKET_TYPE_CHILD,  price: 15, requireSeat: true },
+  { type: TICKET_TYPE_INFANT, price: 0,  requireSeat: false }
 ];
+
+export const MIN_NUMBER_TICKETS = 1;
+
+export const MAX_NUMBER_TICKETS = 25;
+
+export const MIN_ACCOUNT_ID_VALUE = 0;
+
