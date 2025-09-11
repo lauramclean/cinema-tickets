0170518746d31c85ccf0a229623e8af54bd4efc4
Laura Mclean <lauramclean86@gmail.com>
2025-09-11 16:05:21 +0100

    feat: implement logic to calculate total price and number of seats required

--- src/helpers/CalculationHelper.js
+++ src/helpers/CalculationHelper.js
@@ -1,3 +1,5 @@
+import { TICKET_TYPE_MAP } from "../utils/constants.js";
+import logger from "../utils/logger.js";
 
 /**
  * 
@@ -5,6 +7,22 @@
  * @returns {Number} number of seats required
  */
 export const calculateNumSeats = (ticketTypeRequests) => {
+  logger.debug({ message: "In calculateNumSeats()" });
+  
+  let numSeats = 0;
+
+  //for each ticket type that has been requested, obtain from the 
+  //mapping whether a seat is required, if so, multiply it by the number
+  //of tickets requested. 
+  ticketTypeRequests?.forEach(requestedType => {
+    const seatRequired = TICKET_TYPE_MAP.find(i => i.type === requestedType.getTicketType())?.requireSeat;
+    if (seatRequired) {  
+      numSeats += requestedType.getNoOfTickets();
+    }
+  });
+
+  logger.debug({ message: `calculateNumSeats returning with numSeats: ${numSeats}`} ); 
+  return numSeats;    
 }
 
 /**
@@ -13,4 +31,20 @@ export const calculateNumSeats = (ticketTypeRequests) => {
  * @returns {Number} cost of the tickets
  */
 export const calculateTotalCost = (ticketTypeRequests) => {
+  logger.debug({ message: "In calculateTotalCost()" });
+
+  let totalCost = 0;
+
+  //for each ticket type that has been requested, obtain from the 
+  //mapping whether a seat is required, if so, multiply it by the number
+  //of tickets requested. 
+  ticketTypeRequests?.forEach(requestedType => {
+    const ticketPrice = TICKET_TYPE_MAP.find(i => i.type === requestedType.getTicketType())?.price;
+    if (ticketPrice) {  
+      totalCost += requestedType.getNoOfTickets() * ticketPrice;
+    }
+  });
+
+  logger.debug({ message: `calculateTotalCost returning with totalCost: £${totalCost}`} );    
+  return totalCost;    
 }
